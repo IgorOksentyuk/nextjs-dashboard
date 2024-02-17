5f6ce4972d9c6006c2b12c8c73a673404f12f27b
Ihor Oksentiuk <winbka.lem@gmail.com>
2024-02-17 12:36:16 +0200

    Used useForm state hook to add server side form validation

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,24 +2,26 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useSearchParams } from 'next/navigation';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  // const { replace } = useRouter();
+  const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
+
+    params.set('page', '1');
 
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
-
-    // replace(`${pathname}?${params.toString()}`);
-  }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -35,6 +37,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         }}
         defaultValue={searchParams.get('query')?.toString()}
       />
+
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
